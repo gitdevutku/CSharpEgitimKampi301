8b51a29963f86515b9ccd2dd29a0f9d2d09705bc
Utku Berki Baysal <utkuberki21@gmail.com>
2025-04-20 12:04:20 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        #region field variable property farkı
+        //int x;
+        //public int y { get; set; }
+        //void test () {
+        //    int z;
+        //}
+        #endregion
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
+/*
+ * field - variable - property
+ * 
+ */
+/* 
+ * int x -- > field
+ */
